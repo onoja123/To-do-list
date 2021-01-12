f3c91918f25431ddb7dd9dae7a8fbed14b1d98c1
onoja123 <67971490+onoja123@users.noreply.github.com>
2021-01-13 00:47:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,33 @@ loadEvents();
 function loadEvents(){
   document.querySelector('form').addEventListener('submit',submit);
   document.getElementById('clear').addEventListener('click',clearList);
-  // recently added
   document.querySelector('ul').addEventListener('click',deleteOrTick);
 
 }
+// subit data function
+function submit(e){
+  e.preventDefault();
+  let taskList;
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+// add tasks
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+  ul.appendChild(li);
+  document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+// clear the LIST
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
 // deleteTick
 function deleteOrTick(e){
   if(e.target.className == 'delete')
