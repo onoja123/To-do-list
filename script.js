b674a3f68c75f7bb145db173e49155337d21f9cb
onoja123 <67971490+onoja123@users.noreply.github.com>
2021-01-13 00:40:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-const addTodoItemButton = document.querySelector('#add-todo-icon-button')
-const todoItemsContainer = document.querySelector('#todoItems');
-  window.todoItems = [];
-
-addTodoItemButton.addEventListener('click', showPrompt)
-
-function showPrompt(e) {
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+}
+// subit data function
+function submit(e){
   e.preventDefault();
-  const todoItem = prompt('What do you plan on doing?');
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
 
-  const todoItemDiv = document.createElement('div');
-  todoItemDiv.classList.add('todoItem');
-  const inputElement = document.createElement('input')
-  inputElement.type = 'checkbox';
-  const todoItemContentElem = document.createElement('div')
-  todoItemContentElem.classList.add('todoItemContent');
-  todoItemContentElem.innerHTML = todoItem;
-
-  inputElement.addEventListener('click', function(e){
-    console.log(e.target.parentNode)
-    if(e.target.checked){
-      e.target.parentNode.remove();
-    }
-  });
-
-  todoItemDiv.appendChild(inputElement)
-  todoItemDiv.appendChild(todoItemContentElem);
-
-  if(window.todoItems && window.todoItems.length <= 0){
-      todoItemsContainer.innerHTML = '';
-  }
-  window.todoItems.push(todoItem);
-  todoItemsContainer.appendChild(todoItemDiv);
-  
+// add tasks
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+  ul.appendChild(li);
+  document.querySelector('.tasksBoard').style.display = 'block';
 }
