4c295468eb48653e130b972d3abba515fb41e82c
onoja123 <67971490+onoja123@users.noreply.github.com>
2021-01-13 00:45:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ loadEvents();
 // load every event in the page
 function loadEvents(){
   document.querySelector('form').addEventListener('submit',submit);
-  document.getElementById('clear').addEventListener('click',clearList)
+  document.getElementById('clear').addEventListener('click',clearList);
+  // recently added
+  document.querySelector('ul').addEventListener('click',deleteOrTick);
+
 }
-// subit data function
-function submit(e){
-  e.preventDefault();
-  let input = document.querySelector('input');
-  if(input.value != '')
-    addTask(input.value);
-  input.value = '';
+// deleteTick
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
 }
 
-// add tasks
-function addTask(task){
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
-  ul.appendChild(li);
-  document.querySelector('.tasksBoard').style.display = 'block';
+// delete task
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#ff0000";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#2f4f4f";
+  }
 }
